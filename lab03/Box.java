4d58719339fa4c2659ae39d4f970427abf255dfb
seanb2028 <m280480@usna.edu>
2026-01-22 15:30:04 -0500

    Update si211 directory

---
+++ lab03/Box.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class Box
+{
+    // lower left, upper left, and the point
+    private Point l_l;
+    private Point u_l;
+
+    // constructor for Box taking a single point
+    public Box(Point p) { 
+        l_l = new Point(p.pnt_x, p.pnt_y);
+        u_l = new Point(p.pnt_x, p.pnt_y);
+    }
+
+    // constructor for Box taking two initial points
+    public Box(Point a, Point b) {
+        l_l = new Point(Math.min(a.pnt_x, b.pnt_x), Math.min(a.pnt_y, b.pnt_y));
+        u_l = new Point(Math.max(a.pnt_x, b.pnt_x), Math.max(a.pnt_y, b.pnt_y));
+    }
+
+    // expand the bounding box (if needed) to include point p
+    public void growBy(Point p) {
+        // check pnt_x bounds
+        if (p.pnt_x > u_l.pnt_x) { u_l.pnt_x = p.pnt_x; }
+        else if (p.pnt_x < l_l.pnt_x) { l_l.pnt_x = p.pnt_x; }
+        // check pnt_y bounds
+        if (p.pnt_y > u_l.pnt_y) { u_l.pnt_y = p.pnt_y; }
+        else if (p.pnt_y < l_l.pnt_y) { l_l.pnt_y = p.pnt_y; }
+    }
+
+    // given point p in the bounding box, return associated
+    // point in the unit square (see notes); return null if
+    // p is not inside the bounding box.
+    public Point mapIntoUnitSquare(Point p) {
+        return new Point((p.pnt_x - l_l.pnt_x) / (u_l.pnt_x - l_l.pnt_x), (p.pnt_x - l_l.pnt_y) / (u_l.pnt_y - l_l.pnt_y)); 
+    }
+
+    // returns string representation like: 
+    // 2.0 < x < 9.0, 3.0 < y < 7.0
+    public String toString() {
+        return l_l.pnt_x + " < x < " + u_l.pnt_y + ", " + l_l.pnt_y + " < y < " + u_l.pnt_y;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Point p = Point.read(sc);
+        System.out.println(p.toString());
+    }
+}
---
+++ lab03/Point.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Point {
+    // create private fields
+    private double pnt_x;
+    private double pnt_y;
+
+    // constructors for points x and y
+    public Point(double x, double y) {
+        pnt_x = x;
+        pnt_y = y;
+    }
+
+    // reads in x and y and stores the point
+    public static Point read(Scanner sc) {
+        double x = sc.nextDouble();
+        double y = sc.nextDouble();
+        return new Point(x,y);
+    }
+
+    // returns a point as a string
+    public String toString() {
+        return pnt_x + " " + pnt_y;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Point p = read(sc);
+        System.out.println(p.toString());
+    }
+}  
