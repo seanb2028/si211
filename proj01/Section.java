7fd56ea8983f9aebd2bcf0b3ff5f3498f81ad47a
seanb2028 <m280480@usna.edu>
2026-02-05 15:50:22 -0500

    proj01 update

--- proj01/Section.java
+++ proj01/Section.java
@@ -1,7 +1,21 @@
-// our class (struct) for a section
+import java.util.*;
 public class Section {
+  // our class (struct data) for a section
   public String course;
   public int section;
   public String schedule;
   public String room;
+
+  public static void print(Section s) {
+    System.out.println(s.course + " " + s.section + " " + s.schedule + " " + s.room);
+  }
+
+  public static Section read(Scanner sc) {
+    Section s = new Section();
+    s.course = sc.next();
+    s.section = sc.nextInt();
+    s.schedule = sc.next();
+    s.room = sc.next();
+    return s;
+  }
 }
