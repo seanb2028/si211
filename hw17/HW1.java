86c7b1b5dd41012c842affd0696cb7839e98a07a
Becky <m280480@usna.edu>
2026-03-24 21:54:42 -0400

    Completed hw17

--- hw17/HW1.java
+++ hw17/HW1.java
@@ -1,3 +1,6 @@
+/**
+ * Co-author: Sean Beckford
+ */
 import java.util.*;
 import java.io.*;
 
@@ -5,14 +8,32 @@ public class HW1
 {
   public static void main(String[] args)
   {
-    Reader r = new InputStreamReader(System.in);
-    Scanner sc = new Scanner(r);
+    LineNumberReader lnr = null;
+    
+    // File input
+    if (args.length > 0) {
+      // File reader handles bytes -> characters conversion and is a Reader
+      try {
+        lnr = new LineNumberReader(new FileReader(args[0])); 
+      } catch (FileNotFoundException e) {
+        System.out.println("File \"" + args[0] + "\" not found");
+        System.exit(1);
+      }
+    } else { // User input
+      lnr = new LineNumberReader(new InputStreamReader(System.in));
+    }
+
+    Scanner sc = new Scanner(lnr);
+
     try {
       System.out.println(Mystery.compute(sc));
     }
     catch(Exception e)
     {
-      System.out.println("Error " + e.getMessage());
+      System.out.println("Error " + e.getMessage() + " at line " + lnr.getLineNumber());
     }
+    
+    // Always close input streams
+    sc.close();
   }
 }
